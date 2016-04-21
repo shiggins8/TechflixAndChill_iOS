9c86cb360141f6826532ac5b2ba7c78ae90582f2
Scott Higgins <skoty.higgins@gmail.com>
2016-04-21 11:38:52 -0400

    Initial Commit

---
+++ TechflixAndChill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechflixAndChill
+//
+//  Created by Scott Higgins on 4/21/16.
+//  Copyright © 2016 Scott Higgins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
